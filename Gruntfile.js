8f32ea4d518bf46d9cf4af1b734f1bc6a3527660
Alex Vernacchia <alvernacchia@gmail.com>
2013-06-05 00:49:50 -0400

    furthering unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,52 +5,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
     jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {}
-      },
+      all: [
+        'objects/*.js'
+      ],
+      options: { jshintrc: '.jshintrc' },
       gruntfile: {
         src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
       }
     },
     qunit: {
@@ -69,13 +30,11 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['qunit']);
 
 };
--- objects/es5_object.js
+++ objects/es5_object.js
@@ -1,92 +1,92 @@
-var Calculator = (function ComAppCalc(data) {
-	"use strict";
-	var
-		results = data
-		, questionTypes = {
-				'group': 'g'
-				, 'meeting': 'm'
-				, 'interpersonal': 'i'
-				, 'public_speaking': 'p'
-		}
-		, scores = {}
-	;
+define(function() {
+	return function ComAppCalc(data) {
+		"use strict";
+		var
+			results = data
+			, questionTypes = {
+					'group': 'g'
+					, 'meeting': 'm'
+					, 'interpersonal': 'i'
+					, 'public_speaking': 'p'
+			}
+			, scores = {}
+		;
 
-	// function that calculates the individual group scores based on question
-	function getGroupScore( name, getCached ) {
+		// function that calculates the individual group scores based on question
+		function getGroupScore( name, getCached ) {
 
-		if( !results ) {
-			return "No Data";
-		}
-
-		// will always try to get cached scores
-		getCached || ( getCached = true );
-
-		// deciding what to return
-		if( !!getCached && !!scores[ name ] ) {
-
-			// returned cached score if there already, just so it doesn't have to calculate it everytime
-			return scores[ name ];
-
-		} else if( !!questionTypes[ name ] ) {
-
-			// calculating score if needed
-			var score = 18 - sum( [ results[questionTypes[name] + '1'], results[questionTypes[name] + '2'], results[questionTypes[name] + '3'] ] ) + sum( [ results[questionTypes[name] + '3'], results[questionTypes[name] + '4'], results[questionTypes[name] + '5'] ] );
-
-			// caching score
-			scores[ name ] = score;
-
-			// returning socre
-			return score;
-
-		} else {
-
-			// no supposed to be here
-			return "You did something wrong. Try again.";
-
-		}
-	}
-
-	// function that sums all scores to final score and returns it
-	function getPRCA() {
-		return sum( [ getGroupScore( 'group' ), getGroupScore( 'meeting' ), getGroupScore( 'interpersonal' ), getGroupScore( 'public_speaking' ) ] );
-	}
-
-	// function to load data and remove cache
-	function loadNewData( data ) {
-		results = data;
-		scores  = {};
-	}
-
-	function sum( array ) {
-		var sum = 0;
-		if( array.length === 1 ) {
-
-			// return lone array item as a int (will not be dealing with floats as we shouldn't be getting them anyway)
-			sum = parseInt(array);
-
-		} else if( array.length > 1 ) {
-
-			// do the sum
-			for( var i = 0; i < array.length; i++ ) {
-				sum = sum + ( parseInt( array[ i ] ) || 0 );
+			if( !results ) {
+				return "No Data";
 			}
 
-		} else {
+			// will always try to get cached scores
+			getCached || ( getCached = true );
 
-			// doesn't meet requirements
-			sum = false;
+			// deciding what to return
+			if( !!getCached && !!scores[ name ] ) {
 
+				// returned cached score if there already, just so it doesn't have to calculate it everytime
+				return scores[ name ];
+
+			} else if( !!questionTypes[ name ] ) {
+
+				// calculating score if needed
+				var score = 18 - sum( [ results[questionTypes[name] + '1'], results[questionTypes[name] + '2'], results[questionTypes[name] + '3'] ] ) + sum( [ results[questionTypes[name] + '3'], results[questionTypes[name] + '4'], results[questionTypes[name] + '5'] ] );
+
+				// caching score
+				scores[ name ] = score;
+
+				// returning socre
+				return score;
+
+			} else {
+
+				// no supposed to be here
+				return "You did something wrong. Try again.";
+
+			}
 		}
 
-		return sum;
+		// function that sums all scores to final score and returns it
+		function getPRCA() {
+			return sum( [ getGroupScore( 'group' ), getGroupScore( 'meeting' ), getGroupScore( 'interpersonal' ), getGroupScore( 'public_speaking' ) ] );
+		}
+
+		// function to load data and remove cache
+		function loadNewData( data ) {
+			results = data;
+			scores  = {};
+		}
+
+		function sum( array ) {
+			var sum = 0;
+			if( array.length === 1 ) {
+
+				// return lone array item as a int (will not be dealing with floats as we shouldn't be getting them anyway)
+				sum = parseInt(array);
+
+			} else if( array.length > 1 ) {
+
+				// do the sum
+				for( var i = 0; i < array.length; i++ ) {
+					sum = sum + ( parseInt( array[ i ] ) || 0 );
+				}
+
+			} else {
+
+				// doesn't meet requirements
+				sum = false;
+
+			}
+
+			return sum;
+		}
+
+		// returning object
+		return {
+			getGroupScore: getGroupScore
+			, getPRCA: getPRCA
+			, loadData: loadNewData
+		};
 	}
-
-	// returning object
-	return {
-		getGroupScore: getGroupScore
-		, getPRCA: getPRCA
-		, loadData: loadNewData
-	};
 });
-
-return Calculator;
