7c185e63f0f1f59bfc58110e6732053e662a9710
Alex Vernacchia <avernacchia@exacttarget.com>
2013-06-15 10:59:44 -0400

    adding final tests for loading new data into same object

--- test/es5_object.js
+++ test/es5_object.js
@@ -33,6 +33,33 @@ require(['jquery', 'objects/es5_object'], function($, Calculator) {
 		, "p6": 4
 	};
 
+	var sampleData2 = {
+		"g1": 2
+		, "g2": 5
+		, "g3": 1
+		, "g4": 4
+		, "g5": 3
+		, "g6": 5
+		, "i1": 1
+		, "i2": 4
+		, "i3": 2
+		, "i4": 2
+		, "i5": 5
+		, "i6": 5
+		, "m1": 4
+		, "m2": 1
+		, "m3": 2
+		, "m4": 3
+		, "m5": 5
+		, "m6": 3
+		, "p1": 3
+		, "p2": 1
+		, "p3": 3
+		, "p4": 5
+		, "p5": 3
+		, "p6": 4
+	};
+
 	QUnit.start();
 
 	test("object is initialized without 'new' keyword", function() {
@@ -73,4 +100,22 @@ require(['jquery', 'objects/es5_object'], function($, Calculator) {
 		strictEqual( calc.getGroupScore("public_speaking"), 21, 'returned correct public speaking score');
 	});
 
+	test("loading new data and testing calculations", function() {
+		var calc = Calculator( sampleData1 );
+		strictEqual( calc.getPRCA(), 80, 'returned correct PRCA score for sampleData1');
+
+		calc.loadData( sampleData2 );
+		strictEqual( calc.getPRCA(), 90, 'loaded data correctly and returned correct PRCA score for sampleData2');
+		strictEqual( calc.getGroupScore("group"), 22, 'returned correct group discussion score for sampleData2');
+		strictEqual( calc.getGroupScore("meeting"), 22, 'returned correct meetings score for sampleData2');
+		strictEqual( calc.getGroupScore("interpersonal"), 23, 'returned correct interpersonal score for sampleData2');
+		strictEqual( calc.getGroupScore("public_speaking"), 23, 'returned correct public speaking score for sampleData2');
+	});
+
 });
+
+
+
+
+
+
