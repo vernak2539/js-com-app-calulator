5b044afecfa25c31f6d4325b818d79112ebcd497
Alex Vernacchia <avernacchia@exacttarget.com>
2013-09-19 21:07:42 -0400

    adding vendor files for later use

--- app/public/app_marionette/app/main.js
+++ app/public/app_marionette/app/main.js
@@ -1,1 +1,23 @@
-// test
+requirejs.config({
+	baseUrl: '/vendor',
+	paths: {
+		// APP FILES
+		'app-base'   : '../app',
+
+		// VENDOR FILES
+		marionette : 'backbone.marionette',
+	},
+	deps: ['app-base/router'],
+	shim: {
+		'backbone': {
+			deps: [ 'jquery', 'lodash', 'json2' ],
+			exports: 'Backbone'
+		},
+		'jquery': {
+			exports: '$'
+		},
+		'lodash': {
+			exports: '_'
+		}
+	}
+});
