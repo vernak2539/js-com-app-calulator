d9897bf80a8000bcad6b5def026145a8e224437c
Alex Vernacchia <avernacchia@exacttarget.com>
2013-10-05 19:31:23 -0400

    updating amd modules be usable in node

--- objects/es5_object_amd.js
+++ objects/es5_object_amd.js
@@ -1,31 +1,18 @@
-// Uses AMD or browser globals to create a module.
-
-// If you want something that will also work in Node, see returnExports.js
-// If you want to support other stricter CommonJS environments,
-// or if you need to create a circular dependency, see commonJsStrict.js
-
-// Defines a module "amdWeb" that depends another module called "b".
-// Note that the name of the module is implied by the file name. It is best
-// if the file name and the exported global have matching names.
-
-// If the 'b' module also uses this type of boilerplate, then
-// in the browser, it will create a global .b that is used below.
-
-// If you do not want to support the browser global path, then you
-// can remove the `root` use and the passing `this` as the first arg to
-// the top function.
-
-(function ( root, factory ) {
+(function (root, factory) {
 	"use strict";
-
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
-		define([], factory);
+		define(factory);
+	} else if (typeof exports === 'object') {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like enviroments that support module.exports,
+		// like Node.
+		module.exports = factory();
 	} else {
-		// Browser globals
-		root.amdWeb = factory();
+		// Browser globals (root is window)
+		root.returnExports = factory();
 	}
-} ( this, function () {
+}(this, function () {
 	"use strict";
 
 	return function ComAppCalc(data) {
@@ -113,3 +100,7 @@
 		};
 	};
 }));
+
+
+
+
